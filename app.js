7f1ce3187be359a57e268c3edf507750033114ff
Dylan Gore <hello@dylangore.ie>
2021-04-25 10:29:00 +0100

    :sparkles: Add an option to make a user an admin on server start

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ import swaggerJSDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 import swaggerDefinition from './docs/swagger';
 
+import firebase from './services/firebase';
+
 const options = {
   swaggerDefinition,
   // Paths to files containing OpenAPI definitions
@@ -38,3 +40,15 @@ app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.listen(port, function () {
   console.log('Express: FYP API server listening on port ' + port);
 });
+
+//Set admin user if configured
+if (process.env.ADMIN_UID && process.env.ADMIN_UID.length > 0) {
+  // prettier-ignore
+  firebase.auth().setCustomUserClaims(process.env.ADMIN_UID, {role: 'admin'}).then(_user => {
+    // Invalidate the user's current tokens as permissions may have changed
+    firebase.auth().revokeRefreshTokens(process.env.ADMIN_UID).catch((err => console.error(err)))
+    console.log(`Setting user ${process.env.ADMIN_UID} as admin`)
+  }).catch(_err => {
+    console.error(error)
+  });
+}
